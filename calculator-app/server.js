445db33479a3425c8e07c13250eb1642a6be157e
Hamzakhan114610 <princeoptical18@gmail.com>
2026-08-21 12:54:30 +0500

    updated code

--- calculator-app/server.js
+++ calculator-app/server.js
@@ -1,14 +1,21 @@
-// Har operator ki values yahan apni marzi se customize karo
-const add1 = 100, add2 = 50;
+// Har operator ki values yahan customize karo
+const add1 = 100, add2 = 5;
 const sub1 = 13, sub2 = 2;
 const mul1 = 4, mul2 = 5;
-const div1 = 20, div2 = 4;
+const div1 = 20, div2 = 0; // Agar yahan 0 rakhenge toh error aayega
 
 // Calculations
 const additionResult = add1 + add2;
 const subtractionResult = sub1 - sub2;
 const multiplicationResult = mul1 * mul2;
-const divisionResult = div1 / div2;
+
+// Division ke liye condition
+let divisionResult;
+if (div2 === 0) {
+    divisionResult = "Error: Cannot be divided by 0";
+} else {
+    divisionResult = div1 / div2;
+}
 
 // Outputs terminal (CMD) par show honge
 console.log("Addition (" + add1 + " + " + add2 + ") = " + additionResult);
