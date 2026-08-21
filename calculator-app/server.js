26237bbae6b5b9f55bf9be61a0add703f1381d86
Hamzakhan114610 <princeoptical18@gmail.com>
2026-08-21 12:42:54 +0500

    first commit

---
+++ calculator-app/server.js
@@ -0,0 +1,17 @@
+// Har operator ki values yahan apni marzi se customize karo
+const add1 = 100, add2 = 50;
+const sub1 = 13, sub2 = 2;
+const mul1 = 4, mul2 = 5;
+const div1 = 20, div2 = 4;
+
+// Calculations
+const additionResult = add1 + add2;
+const subtractionResult = sub1 - sub2;
+const multiplicationResult = mul1 * mul2;
+const divisionResult = div1 / div2;
+
+// Outputs terminal (CMD) par show honge
+console.log("Addition (" + add1 + " + " + add2 + ") = " + additionResult);
+console.log("Subtraction (" + sub1 + " - " + sub2 + ") = " + subtractionResult);
+console.log("Multiplication (" + mul1 + " * " + mul2 + ") = " + multiplicationResult);
+console.log("Division (" + div1 + " / " + div2 + ") = " + divisionResult);
