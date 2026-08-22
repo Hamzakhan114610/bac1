ce33a984b2a82b09f75944b073127229d5cb8018
kodexsolution <princeoptical18@gmail.com>
2026-08-22 17:59:17 +0500

    its private
    Added error handling for division by zero in server.js.

--- calculator-app/server.js
+++ calculator-app/server.js
@@ -1,15 +1,15 @@
-// Har operator ki values yahan customize karo
+
 const add1 = 100, add2 = 5;
 const sub1 = 13, sub2 = 2;
 const mul1 = 4, mul2 = 5;
-const div1 = 20, div2 = 0; // Agar yahan 0 rakhenge toh error aayega
+const div1 = 20, div2 = 0;
 
-// Calculations
+
 const additionResult = add1 + add2;
 const subtractionResult = sub1 - sub2;
 const multiplicationResult = mul1 * mul2;
 
-// Division ke liye condition
+
 let divisionResult;
 if (div2 === 0) {
     divisionResult = "Error: Cannot be divided by 0";
